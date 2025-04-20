e4128b16e3bc925f71682fa0d7bf960f4be50782
Leon Petrinos <leon.petrinos@icloud.com>
2025-04-20 17:12:18 -0400

    Python implementation of merge sort

--- src/sorting/merge_sort/merge_sort.py
+++ src/sorting/merge_sort/merge_sort.py
@@ -1,1 +1,56 @@
 """Merge Sort Algorithm."""
+
+from typing import Any
+
+
+def merge_sort(array: list[Any]) -> list[Any]:
+    """Sorts the input array using the merge sort algorithm.
+
+    Parameters
+    ----------
+    array : list[Any]
+        The list of comparable elements to sort.
+
+    Returns
+    -------
+    list[Any]
+        A new list, sorted in ascending order.
+
+    """
+    n = len(array)
+    if n <= 1:
+        return array
+    q = n // 2
+    return merge(merge_sort(array[:q]), merge_sort(array[q:]))
+
+
+def merge(left: list[Any], right: list[Any]) -> list[Any]:
+    """Merge two sorted lists into a single sorted list.
+
+    Parameters
+    ----------
+    left : list[Any]
+        The first sorted list.
+    right: list[Any]
+        The second sorted list.
+
+    Returns
+    -------
+    list[Any]
+        A new list that has merged left and right.
+
+    """
+    n1 = len(left)
+    n2 = len(right)
+    res = []
+    i = j = 0
+    while i < n1 and j < n2:
+        if left[i] < right[j]:
+            res.append(left[i])
+            i += 1
+        else:
+            res.append(right[j])
+            j += 1
+    res.extend(left[i:])
+    res.extend(right[j:])
+    return res
