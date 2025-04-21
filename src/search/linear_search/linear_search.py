a3f70c0932928db5bdbbf6ce3f0b3cba88f5d55d
maximilien_notz <max@pleinchamps.ch>
2025-04-21 20:21:25 +0200

    Adding python implementation

--- src/search/linear_search/linear_search.py
+++ src/search/linear_search/linear_search.py
@@ -1,1 +1,24 @@
 """Linear Search Algorithm."""
+
+
+def linear_search(array: list[int], target: int) -> int:
+    """In-place implementation of linear search.
+
+    Parameters
+    ----------
+    array : list[Any]
+        The list of variable.
+    target : Any
+        The element to be searched
+
+    Returns
+    -------
+    int :
+        The same list, sorted in ascending order.
+
+    """
+    for i in range(array):
+        if array[i] == target:
+            return i
+
+    return -1
